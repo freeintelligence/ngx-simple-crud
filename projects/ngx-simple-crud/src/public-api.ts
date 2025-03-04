4694d0d77bf60cc77dfe5a029634210046b5cbc3
Luis Arriaza <larriaza@numerus.cl>
2025-03-03 23:59:44 -0600

    Se reinicia el paquete..

--- projects/ngx-simple-crud/src/public-api.ts
+++ projects/ngx-simple-crud/src/public-api.ts
@@ -2,14 +2,4 @@
  * Public API Surface of ngx-simple-crud
  */
 
-export * from './lib/simple-crud.module';
-
-export * from './lib/interfaces/button.interface';
-export * from './lib/interfaces/field.interface';
-export * from './lib/interfaces/header.interface';
-export * from './lib/interfaces/info-column.interface';
-export * from './lib/interfaces/paginator.interface';
-export * from './lib/interfaces/service.interface';
-
-export * from './lib/components/resource-list/resource-list.component';
-export * from './lib/components/data-table/data-table.component';
+export * from './lib-old/interfaces/info-column.interface';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SimpleCrudModule } from 'ngx-simple-crud';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    SimpleCrudModule,
-  ],
-  providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [],
 })
 export class AppModule {}
