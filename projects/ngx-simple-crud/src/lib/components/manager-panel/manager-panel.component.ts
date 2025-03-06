f04cc149301427a78057a2989744a5dad7f603f2
Luis Arriaza <free04381@gmail.com>
2025-03-06 01:02:44 -0300

    Prueba..

--- projects/ngx-simple-crud/src/lib/components/manager-panel/manager-panel.component.ts
+++ projects/ngx-simple-crud/src/lib/components/manager-panel/manager-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ManagerPanelParameters } from './manager-panel.parameters';
 import { FormComponent } from 'ngx-simple-forms';
 
